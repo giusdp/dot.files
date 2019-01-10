2c891ec1d1c7fa8f7da95f88c6e7ed7be6c94b2b
Giuseppe De Palma <depalma.gsp@gmail.com>
2019-01-10 21:20:58 +0100

    updated polybar

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -1,10 +1,9 @@
-#!/usr/bin/env bash
-
-# Terminate already running bar instances
+# nate already running bar instances
 killall -q polybar
 
 # Wait until the processes have been shut down
-while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+while pgrep -x polybar >/dev/null; do sleep 1; done
 
 # Launch bar1 and bar2
-polybar base -r &
+#polybar topl &
+polybar -r top &
